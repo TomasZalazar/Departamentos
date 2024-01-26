c355d575b693bd786299fe24db890b945b81fb53
gonzamiguel <gonzalo.augustin.miguel@gmail.com>
2024-01-26 18:00:29 -0300

    NavBar terminada

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -19,15 +19,15 @@ export default function Example() {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden pt-8">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block h-8 w-8" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block h-8 w-8" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
@@ -40,13 +40,13 @@ export default function Example() {
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:flex">
-                  <div className="flex space-x-4">
+                  <div className="flex flex-row justify-center items-center space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-hueso-800  text-white ' : 'text-gray-300 hover:bg-hueso-800 hover:text-white',
                           'rounded-md px-3 py-2 text-xl '
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -59,15 +59,15 @@ export default function Example() {
               </div>
             </div>
           </div>
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+          <Disclosure.Panel className="sm:hidden pt-8">
+            <div className="space-y-1 px-2 pb-3 pt-4 bg-[#1F1F1F]">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-hueso-800 text-white' : 'text-gray-300 hover:bg-hueso-800 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,68 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        naranja: {
+          100: "#ffead4",
+          200: "#ffcfab",
+          300: "#ffb282",
+          400: "#ff9660",
+          500: "#cc5533", // saturación por defecto
+          600: "#993f1f",
+          700: "#662b0d",
+          800: "#331606",
+          900: "#000000",
+        },
+        hueso: {
+          100: "#ffffff",
+          200: "#fcf5f3",
+          300: "#f6ebe9",
+          400: "#f1e0df",
+          500: "#e5cacc",
+          600: "#b8a49f",
+          700: "#8a7870",
+          800: "#5d4b41",
+          900: "#2f2521",
+        },
+        marron: {
+          100: "#ffdbcc",
+          200: "#ffad85",
+          300: "#ff7f3f",
+          400: "#ff6b1a",
+          500: "#6a2c1a", // saturación por defecto
+          600: "#4e1e13",
+          700: "#331106",
+          800: "#1b0900",
+          900: "#000000",
+        },
+        gris: {
+          100: "#e6e6e6",
+          200: "#b3b3b3",
+          300: "#808080",
+          400: "#666666",
+          500: "#8c7e6c", // saturación por defecto
+          600: "#595959",
+          700: "#404040",
+          800: "#262626",
+          900: "#000000",
+        },
+        grisclaro: {
+          100: "#ffffff",
+          200: "#e6e6e6",
+          300: "#b3b3b3",
+          400: "#808080",
+          500: "#c8c4b4", // saturación por defecto
+          600: "#a6a299",
+          700: "#7a7874",
+          800: "#4d4b47",
+          900: "#21211e",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
